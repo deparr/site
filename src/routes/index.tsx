f4ee5f8b6b0a356ab262c4de7aaf384b541834a8
deparr <dparrott@protonmail.com>
2024-08-21 15:41:58 -0600

    .

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,11 @@ import { Projects } from "~/components/Projects";
 import projectList from "../../shared/projects.json";
 
 export default function Index() {
-	return (
-		<main id="main">
-			<h1>Welcome!</h1>
-			<Projects projs={projectList as Project[]} />
-			<Projects projs={projectList as Project[]} filter="featured" />
-		</main>
-	);
+    return (
+        <main id="main">
+            <h1>Welcome!</h1>
+            <Projects projs={projectList as Project[]} />
+            <Projects projs={projectList as Project[]} filter="featured" />
+        </main>
+    );
 }
