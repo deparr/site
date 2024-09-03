ef101f6b51e44771bb091539f6449acc76a52fff
deparr <dparrott@protonmail.com>
2024-09-02 21:50:41 -0600

    try to style i guess

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,17 +1,24 @@
 /// <reference types="@solidjs/start/env" />
 type Project = {
-	display_name?: string;
-	name?: string;
-	desc?: string;
-	tech?: string[];
-	timeline?: string;
-	images?: string[];
-	type?: string;
-	repo?: string;
+    display_name?: string;
+    name?: string;
+    desc?: string;
+    tech?: string[];
+    timeline?: string;
+    images?: string[];
+    type?: string;
+    repo?: string;
 }
 
 type ProjectListProps = {
-	projs: Project[];
-	filter?: string;
-	linkToAll?: boolean;
+    projs: Project[];
+    filter?: string;
+    linkToAll?: boolean;
 }
+
+type HeroProps = {
+    main: string;
+    sub?: string;
+    upper?: boolean;
+}
+
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
 import { Projects } from "~/components/Projects";
+import Hero from "~/components/Hero";
 import projectList from "../../shared/projects.json";
 import g from "~/global.module.css";
 
 export default function Index() {
     return (
         <main class={g.main}>
-            <h1 class={`${g.h1}`}>Welcome!</h1>
+            <Hero main="David Parrott" upper={true} />
             <Projects projs={projectList as Project[]} />
-            <Projects projs={projectList as Project[]} filter="featured" />
         </main>
     );
 }
